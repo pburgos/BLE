030753f5e1ca01883835043b2068b6b8662e8f41
Pablo Burgos <pablo.burgos.avila@gmail.com>
2014-01-31 11:23:20 -0300

    BLE modified to show RSSI

---
+++ BluetoothLeGatt/build/source/buildConfig/debug/com/example/bluetooth/le/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bluetooth.le;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
